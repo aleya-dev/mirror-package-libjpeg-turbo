0306958d5f1730a0fbb0b871655e2e63380f4c38
Robin Degen <robindegen@gmail.com>
2023-12-16 22:39:40 +0100

    Add conanfile

---
+++ conanfile.py
@@ -0,0 +1,50 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain
+from conan.tools.files import rmdir, collect_libs
+import os
+
+
+required_conan_version = ">=2.0"
+
+
+class LibJpegTurboConan(ConanFile):
+    name = "libjpeg-turbo"
+    version = "3.0.1"
+    python_requires = "aleya-conan-base/1.3.0@aleya/public"
+    python_requires_extend = "aleya-conan-base.AleyaCmakeBase"
+    ignore_cpp_standard = True
+
+    exports_sources = "source/*"
+
+    options = {
+        "shared": [False, True],
+        "fPIC": [False, True]
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True
+    }
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.variables["ENABLE_SHARED"] = self.options.shared
+        tc.variables["ENABLE_STATIC"] = not self.options.shared
+        tc.generate()
+
+    def package(self):
+        super().package()
+
+        rmdir(self, os.path.join(self.package_folder, "bin"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_module_file_name", "JPEG")
+        self.cpp_info.set_property("cmake_file_name", "libjpeg-turbo")
+
+        self.cpp_info.components["turbojpeg"].libs = collect_libs(self)
+        self.cpp_info.components["turbojpeg"].set_property("cmake_target_name", "libjpeg-turbo::turbojpeg-static")
